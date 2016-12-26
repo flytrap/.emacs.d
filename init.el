b6cb060eb485c3d82b24168e74ccc12f42ccaae1
flytrap <hiddenstat@gmail.com>
2016-12-26 23:12:16 +0800

    add yasnippet popup auto-complate

--- init.el
+++ init.el
@@ -20,10 +20,17 @@
 
 ;(add-to-list 'load-path "~/.emacs.d/site-lisp/jdee-2.4.1/lisp")
 ;(load "jde")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/")
 (add-to-list 'load-path "~/.emacs.d/site-lisp/ecb/")
-(load-file "~/.emacs.d/site-lisp/ecb/ecb.el")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/yasnippet")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/auto-complete")
+;(load-file "~/.emacs.d/site-lisp/ecb/ecb.el")
 (require 'ecb)
-(setq ecb-use-speedbar-instead-native-tree-buffer t)
+(require 'yasnippet)
+(yas-global-mode 1)
+(require 'auto-complete-config)
+(ac-config-default)
+;(setq ecb-use-speedbar-instead-native-tree-buffer t)
 ;(setq ecb-auto-activate t
 ;      ecb-tip-of-the-day nil)
 (custom-set-variables
@@ -31,11 +38,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ecb-options-version "2.50")
- '(ecb-source-path
+ '(ecb-layout-window-sizes
    (quote
-    (("/" "/")
-     "~/Documents/code/git/cnblogs"))))
+    (("left8"
+      (ecb-directories-buffer-name 0.31543624161073824 . 0.2777777777777778)
+      (ecb-sources-buffer-name 0.31543624161073824 . 0.2222222222222222)
+      (ecb-methods-buffer-name 0.31543624161073824 . 0.3055555555555556)
+      (ecb-history-buffer-name 0.31543624161073824 . 0.16666666666666666)))))
+ '(ecb-options-version "2.50")
+ '(ecb-source-path (quote (("/" "/") "~/Documents/code/git/cnblogs"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
